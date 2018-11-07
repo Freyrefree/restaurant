199865eb1d42fc4bf74d18e1a1bd21cdbcfab5c0
Diego <dgutierrez@aiko.com.mx>
2018-11-06 18:00:19 -0600

    ok

--- core/app/view/lla_agregafactura-view.php
+++ core/app/view/lla_agregafactura-view.php
@@ -1,27 +1,23 @@
 <?php
 
-$TComprobante   =$_POST['tc'];
-$folio          ="";
-$serie          ="";
-$emisor         =$_POST['r_emisor'];
-$receptor       =$_POST['r_receptor'];
-$formaPago      =$_POST['forma'];
-$metodoPago     =$_POST['metodo'];
-$fecha          =$_POST['fecha'];
-$moneda         =$_POST['moneda'];
-$condicion      =$_POST['condicion'];
-$idReceptor     =$_POST['idReceptor'];
-$usoCFDI        =$_POST['usocfdi'];
-
+$TComprobante   = $_POST['tc'];
+$folio          = "";
+$serie          = "";
+$emisor         = $_POST['r_emisor'];
+$receptor       = $_POST['r_receptor'];
+$formaPago      = $_POST['forma'];
+$metodoPago     = $_POST['metodo'];
+$fecha          = $_POST['fecha'];
+$moneda         = $_POST['moneda'];
+$condicion      = $_POST['condicion'];
+$idReceptor     = $_POST['idReceptor'];
+$usoCFDI        = $_POST['usocfdi'];
 
 
 
 $grade = new CategoryData();
 $grade->name = $_POST["name"];
 $grade->add();
-
-
-
 
 
 
